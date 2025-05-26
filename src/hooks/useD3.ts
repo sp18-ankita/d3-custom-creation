1830fac48a5c9ecd0f68a5ea47b65cfb1f25ac97
sp18-ankita <ankita.agarwala@sparkeighteen.com>
2025-05-26 17:01:49 +0530

    Updated hook

--- src/hooks/useD3.ts
+++ src/hooks/useD3.ts
@@ -1,9 +1,9 @@
 import * as d3 from 'd3';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, type DependencyList } from 'react';
 
 export const useD3 = (
   renderFn: (svg: d3.Selection<SVGSVGElement | null, unknown, null, undefined>) => void,
-  deps: unknown[],
+  deps: DependencyList,
 ) => {
   const ref = useRef<SVGSVGElement | null>(null);
 
@@ -11,7 +11,7 @@ export const useD3 = (
     const svg = d3.select(ref.current);
     svg.selectAll('*').remove();
     renderFn(svg);
-  }, [renderFn, ...deps]);
+  }, deps);
 
   return ref;
 };
