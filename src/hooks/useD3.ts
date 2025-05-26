5bb8e0c68df9d36cf724d32beb7551850ec6289b
sp18-ankita <ankita.agarwala@sparkeighteen.com>
2025-05-26 17:00:08 +0530

    Fixed hook

--- src/hooks/useD3.ts
+++ src/hooks/useD3.ts
@@ -1,9 +1,9 @@
 import * as d3 from 'd3';
-import { useEffect, useRef, type DependencyList } from 'react';
+import { useEffect, useRef } from 'react';
 
 export const useD3 = (
   renderFn: (svg: d3.Selection<SVGSVGElement | null, unknown, null, undefined>) => void,
-  deps: DependencyList,
+  deps: unknown[],
 ) => {
   const ref = useRef<SVGSVGElement | null>(null);
 
@@ -11,7 +11,7 @@ export const useD3 = (
     const svg = d3.select(ref.current);
     svg.selectAll('*').remove();
     renderFn(svg);
-  }, deps);
+  }, [renderFn, ...deps]);
 
   return ref;
 };
