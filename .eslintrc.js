ab0b7368919a57929438e41077de89a8d280a446
sp18-ankita <ankita.agarwala@sparkeighteen.com>
2025-05-26 16:58:43 +0530

    Updatd husky rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,38 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      project: './tsconfig.json',
-    },
-    plugins: ['@typescript-eslint', 'prettier', 'import'],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:import/errors',
-      'plugin:import/warnings',
-      'plugin:import/typescript',
-      'plugin:prettier/recommended'
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/no-use-before-define': ['error'],
+
+    'prefer-const': 'error',
+
+    '@typescript-eslint/no-floating-promises': 'error',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+
+    'import/order': [
+      'warn',
+      {
+        groups: [['builtin', 'external'], 'internal', ['parent', 'sibling', 'index']],
+      },
     ],
-    rules: {
-      'prettier/prettier': 'error',
-  
-      '@typescript-eslint/explicit-function-return-type': 'warn',
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/consistent-type-imports': 'error',
-      '@typescript-eslint/no-use-before-define': ['error'],
-
-      'prefer-const': 'error',
-
-      '@typescript-eslint/no-floating-promises': 'error',
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
-  
-      'import/order': ['warn', {
-        groups: [['builtin', 'external'], 'internal', ['parent', 'sibling', 'index']]
-      }]
-    }
-  };
-  
+  },
+};
