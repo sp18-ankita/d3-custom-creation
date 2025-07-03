2fdc6bcb42125cc2cf556385e110a6faddd79e3d
sp18-ankita <ankita.agarwala@sparkeighteen.com>
2025-07-03 16:04:34 +0530

    Removed eslintignore updated config

--- eslint.config.js
+++ eslint.config.js
@@ -4,20 +4,24 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
-  files: ['**/*.{ts,tsx}'],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
+export default tseslint.config(
+  {
+    ignores: ['coverage/**', 'dist/**', 'build/**', 'node_modules/**', 'e2e/**'],
   },
-  plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
   },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-  },
-  ignores: ['coverage/**', 'dist/**', 'build/**', 'node_modules/**', 'e2e/**'],
-});
+);
